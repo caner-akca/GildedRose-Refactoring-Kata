bf3ce5ba0ed7b730f59b8ee74972cff5722dde9d
caner <caner.akca@kontist.com>
2022-11-16 14:49:54 +0300

    Create tests for understanding the behaviour of the legacy code

--- TypeScript/test/mocha/gilded-rose.spec.ts
+++ TypeScript/test/mocha/gilded-rose.spec.ts
@@ -1,10 +1,45 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+type item = {
+  name: string;
+  sellIn: number;
+  quality: number;
+  numberOfSellIn: number;
+  outputQuality: number;
+};
+
+describe("Gilded Rose", () => {
+  it("describe behaviour of the legacy code", () => {
+    const itemNames = [
+      "Normal Item",
+      "Aged Brie",
+      "Backstage passes to a TAFKAL80ETC concert",
+      "Sulfuras, Hand of Ragnaros",
+    ];
+
+    const tests: item[] = [];
+
+    const [minSellIn, maxSellIn] = [-1, 12];
+    const [minQuality, maxQuality] = [-1, 51];
+
+    for (const name of itemNames) {
+      for (let sellIn = minSellIn; sellIn <= maxSellIn; sellIn++) {
+        for (let quality = minQuality; quality <= maxQuality; quality++) {
+          const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+          const items = gildedRose.updateQuality();
+
+          const numberOfSellIn = items[0].sellIn;
+          const outputQuality = items[0].quality;
+
+          tests.push({
+            name,
+            sellIn,
+            quality,
+            numberOfSellIn,
+            outputQuality,
+          });
+        }
+      }
+    }
   });
 });
