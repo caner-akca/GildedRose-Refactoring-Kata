e496eaba860f953dbd291864ba1e3e173dcad90e
caner <caner.akca@kontist.com>
2022-11-17 12:38:00 +0300

    Refactor constant values

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -16,51 +16,61 @@ export enum ItemTypes {
   SULFURAS = "Sulfuras, Hand of Ragnaros",
 }
 
+export const values = {
+  quality: { MAX_VALUE: 50, MIN_VALUE: 0 },
+  sellIn: { MAX_VALUE: 50, MIN_VALUE: 0 },
+};
+
 const updateNormal = (item: Item): Item => {
-  if (item.quality > 0) {
-    item.quality = item.quality - 1;
+  if (item.quality > values.quality.MIN_VALUE) {
+    item.quality--;
   }
 
-  item.sellIn = item.sellIn - 1;
+  item.sellIn--;
 
-  if (item.sellIn < 0 && item.quality > 0) {
-    item.quality = item.quality - 1;
+  if (
+    item.sellIn < values.sellIn.MIN_VALUE &&
+    item.quality > values.quality.MIN_VALUE
+  ) {
+    item.quality--;
   }
   return item;
 };
 
 const updateBrie = (item: Item): Item => {
-  if (item.quality < 50) {
-    item.quality = item.quality + 1;
+  item.sellIn--;
+  if (item.quality < values.quality.MAX_VALUE) {
+    item.quality++;
   }
 
-  item.sellIn = item.sellIn - 1;
-
-  if (item.sellIn < 0 && item.quality < 50) {
-    item.quality = item.quality + 1;
+  if (
+    item.sellIn < values.sellIn.MIN_VALUE &&
+    item.quality < values.quality.MAX_VALUE
+  ) {
+    item.quality++;
   }
   return item;
 };
 
 const updateBackStage = (item: Item): Item => {
-  if (item.quality < 50) {
-    item.quality = item.quality + 1;
+  if (item.quality < values.quality.MAX_VALUE) {
+    item.quality++;
 
-    if (item.sellIn < 11 && item.quality < 50) {
-      item.quality = item.quality + 1;
+    if (item.sellIn < 11 && item.quality < values.quality.MAX_VALUE) {
+      item.quality++;
     }
-    if (item.sellIn < 6 && item.quality < 50) {
-      item.quality = item.quality + 1;
+    if (item.sellIn < 6 && item.quality < values.quality.MAX_VALUE) {
+      item.quality++;
     }
   }
-  item.sellIn = item.sellIn - 1;
-  if (item.sellIn < 0) {
-    item.quality = item.quality - item.quality;
+  item.sellIn--;
+  if (item.sellIn < values.sellIn.MIN_VALUE) {
+    item.quality = 0;
   }
   return item;
 };
 
-const update = (item: Item): Item => {
+const updateItem = (item: Item): Item => {
   if (item.name === ItemTypes.BRIE) {
     return updateBrie(item);
   }
@@ -72,6 +82,7 @@ const update = (item: Item): Item => {
   if (item.name === ItemTypes.SULFURAS) {
     return item;
   }
+
   return updateNormal(item);
 };
 export class GildedRose {
@@ -82,7 +93,7 @@ export class GildedRose {
   }
 
   updateQuality() {
-    this.items.forEach((item) => update(item));
+    this.items.forEach((item) => updateItem(item));
     return this.items;
   }
 }
