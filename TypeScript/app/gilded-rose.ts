ae3c493a4ce50448c76386dfb4bb25b82fa9e2dd
caner <caner.akca@kontist.com>
2022-11-17 11:00:29 +0300

    Refactor logic into different functions

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -9,6 +9,53 @@ export class Item {
     this.quality = quality;
   }
 }
+
+export enum ItemTypes {
+  BRIE = "Aged Brie",
+  BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert",
+  SULFURAS = "Sulfuras, Hand of Ragnaros",
+}
+
+function updateNormal(item: Item): void {
+  if (item.quality > 0) {
+    item.quality = item.quality - 1;
+  }
+
+  item.sellIn = item.sellIn - 1;
+
+  if (item.sellIn < 0 && item.quality > 0) {
+    item.quality = item.quality - 1;
+  }
+}
+
+function updateBrie(item: Item): void {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+  }
+
+  item.sellIn = item.sellIn - 1;
+
+  if (item.sellIn < 0 && item.quality < 50) {
+    item.quality = item.quality + 1;
+  }
+}
+
+function updateBackStage(item: Item): void {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+
+    if (item.sellIn < 11 && item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+    if (item.sellIn < 6 && item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+  }
+  item.sellIn = item.sellIn - 1;
+  if (item.sellIn < 0) {
+    item.quality = item.quality - item.quality;
+  }
+}
 export class GildedRose {
   items: Array<Item>;
 
@@ -18,51 +65,18 @@ export class GildedRose {
 
   updateQuality() {
     for (const item of this.items) {
-      if (
-        item.name != "Aged Brie" &&
-        item.name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
+      switch (item.name) {
+        case ItemTypes.BRIE:
+          updateBrie(item);
+          continue;
+        case ItemTypes.BACKSTAGE:
+          updateBackStage(item);
+          continue;
+        case ItemTypes.SULFURAS:
+          continue;
+        default:
+          updateNormal(item);
+          continue;
       }
     }
 
